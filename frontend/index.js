27e9f3519d9c0293babf93d52cafa98a77fdc378
SladeBennett <slade8colt@gmail.com>
2024-02-01 00:04:14 -0700

    complete

--- frontend/index.js
+++ frontend/index.js
@@ -5,6 +5,102 @@ async function sprintChallenge5() { // Note the async keyword, in case you wish 
   const currentYear = new Date().getFullYear()
   footer.textContent = `© BLOOM INSTITUTE OF TECHNOLOGY ${currentYear}`
 
+
+
+  const reqA = await axios.get('http://localhost:3003/api/learners')
+  const reqB = await axios.get('http://localhost:3003/api/mentors')
+
+  document.querySelector('.info').textContent = 'No learner is selected'
+
+
+  let learnArr = reqA.data
+  let mentArr = reqB.data
+
+  learnArr.forEach((learner) => {
+    mentArr.forEach((mentor) => {
+      for (let i = 0; i < learner.mentors.length; i++) {
+        if (learner.mentors[i] === mentor.id) {
+          learner.mentors[i] = `${mentor.firstName} ${mentor.lastName}`
+        }
+      }
+    })
+  })
+
+  const theCards = document.querySelector('.cards')
+  function getLearnerCard(learner) {
+    const learnerCard = document.createElement('div')
+    learnerCard.classList.add('card')
+    const NameTag = document.createElement('h3')
+    const Email = document.createElement('div')
+    const MentList = document.createElement('h4')
+    MentList.classList.add('closed')
+    const MentNames = document.createElement('ul')
+
+
+    learner.mentors.forEach((person) => {
+      let individual = document.createElement('li')
+      individual.textContent = person
+      MentNames.appendChild(individual)
+    })
+
+
+    NameTag.textContent = learner.fullName
+    Email.textContent = learner.email
+    MentList.textContent = 'Mentors'
+
+
+
+    const handleClick = () => {
+      const AmazonPrimeMember = document.querySelector('.selected')
+      if (AmazonPrimeMember) {
+        document.querySelector('.selected h3').textContent = document.querySelector('.selected h3').textContent.split(',')[0]
+        document.querySelector('.selected').classList.remove('selected')
+        document.querySelector('.info').textContent = 'No learner is selected'
+      } 
+      if (AmazonPrimeMember !== learnerCard) {
+        learnerCard.classList.add('selected')
+        learnerCard.querySelector('.selected h3').textContent = `${learner.fullName}, ID ${learner.id}`
+        document.querySelector('.info').textContent = `The selected learner is ${learner.fullName}`
+      }
+
+    }
+
+
+
+    const handleClick2 = (event) => {
+      if (document.querySelector('.selected') === learnerCard) {
+        event.stopPropagation()
+      }
+      event.target.classList.toggle('closed')
+      event.target.classList.toggle('open')
+    }
+
+
+
+
+    learnerCard.addEventListener('click', handleClick)
+    MentList.addEventListener('click', handleClick2)
+
+
+    learnerCard.appendChild(NameTag)
+    learnerCard.appendChild(Email)
+    learnerCard.appendChild(MentList)
+    learnerCard.appendChild(MentNames)
+
+    theCards.appendChild(learnerCard)
+  }
+  learnArr.forEach((person) => {
+    getLearnerCard(person)
+  })
+
+
+
+
+
+
+
+
+
   // 👆 WORK WORK ABOVE THIS LINE 👆
 }
 
